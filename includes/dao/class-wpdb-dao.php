ed77915f5a8e7f50f3c8a6c5aa9bd5e85bff8a09
Kai Pfeiffer <kp@loworx.com>
2025-03-16 18:04:17 +0100

    adjusted to changed interface

--- includes/dao/class-wpdb-dao.php
+++ includes/dao/class-wpdb-dao.php
@@ -2,33 +2,44 @@
 
 namespace Loworx\Ridepool;
 
-use Kaipfeiffer\Tramp\Interfaces\DAOConnector;
+use Kaipfeiffer\Tramp\Interfaces\DaoConnectorInterface;
 
-class WPDB_DAO implements DAOConnector
+class WPDB_DAO implements DaoConnectorInterface
 {
 
+
+    /**
+     * table_name
+     *
+     * @var     string
+     * @since   1.0.0
+     */
     protected $table;
 
 
-    public function __construct(string $table)
-    {
-        $this->table = $table;
-    }
+    /**
+     * prefix  for table name
+     *
+     * @var     string
+     * @since   1.0.0
+     */
+    protected $prefix = 'tramp_';
+
 
     /**
-     * @param int|null
-     * @param int|null
+     * PROTECTED METHODS
      */
-    public function read($id = null, $page = null)
-    {
-        return $this->table;
-    }
 
-    public function read_by(array $query, $page = null) {}
-
-    public function create(array $row) {}
-
-    public function create_keys(array $keys, string $type)
+    /**
+     * create keys
+     * 
+     * creates the keys for the WP-dbDelta query
+     * 
+     * @param   array
+     * @param   string
+     * @since   1.0.0
+     */
+    protected function create_keys(array $keys, string $type)
     {
         $key_list   = array();
         foreach ($keys as $key) {
@@ -39,69 +50,101 @@ class WPDB_DAO implements DAOConnector
 
 
     /**
+     * create string definition
      * 
+     * creates the definition for string columns for the WP-dbDelta query
+     * 
+     * @param   array
+     * @since   1.0.0
      */
-    public function create_table(array $data)
+    protected function create_string_definition(array $column_data)
     {
-        global $wpdb;
-        $has_auto_increment = false;
-        $table_name         = $wpdb->prefix . $data['tablename'];
-        $wpdb_collate       = $wpdb->collate;
-        $columns            =
-            $keys           = array();
+        switch (true) {
+            case 10000   <   (int)($column_data['length'] ?? 0): {
+                    $column_data['type']    = ' text';
+                    break;
+                }
+            case 0   <   (int)($column_data['length'] ?? 0): {
+                    $column_data['type']    = sprintf(' varchar(%d)', $column_data['length']);
+                    break;
+                }
+            default:
+                $column_data['type']    = ' varchar(100)';
+        }
+        return $column_data['type'];
+    }
 
-        foreach ($data['column_types'] as $column_name => $column_data) {
+
+    /**
+     * create integer definition
+     * 
+     * creates the definition for integer columns for the WP-dbDelta query
+     * 
+     * @param   array
+     * @since   1.0.0
+     */
+    protected function create_integer_definition(array $column_data)
+    {
+        switch (true) {
+            case 11 < (int)($column_data['length'] ?? 0): {
+                    $column_data['type']    = ' bigint(20)%s';
+                    break;
+                }
+            case 9 < (int)($column_data['length'] ?? 0): {
+                    $column_data['type']    = ' int(11)%s';
+                    break;
+                }
+            case 6 < (int)($column_data['length'] ?? 0): {
+                    $column_data['type']    = ' mediumint(9)%s';
+                    break;
+                }
+            case 4 < (int)($column_data['length'] ?? 0): {
+                    $column_data['type']    = ' smallint(6)%s';
+                    break;
+                }
+            case 0 < (int)($column_data['length'] ?? 0): {
+                    $column_data['type']    = ' tinyint(4)%s';
+                    break;
+                }
+            default:
+                $column_data['type']    = ' int(11)%s';
+        }
+        $column_data['type'] = sprintf($column_data['type'], ($column_data['signed'] ?? true) ? '' : ' unsigned');
+        return $column_data['type'];
+    }
+
+
+    /**
+     * create columns
+     * 
+     * creates the keys for the WP-dbDelta query
+     * 
+     * @param   array
+     * @param   string
+     * @since   1.0.0
+     */
+    protected function create_columns(array $columns)
+    {
+        $column_list        = array();
+        $has_auto_increment = false;
+
+        foreach ($columns as $column_name => $column_data) {
             switch ($column_data['type']) {
                 case 'int':
                 case 'integer': {
-                        switch (true) {
-                            case 11 < (int)($column_data['length'] ?? 0): {
-                                    $column_data['type']    = ' bigint(20)%s';
-                                    break;
-                                }
-                            case 9 < (int)($column_data['length'] ?? 0): {
-                                    $column_data['type']    = ' int(11)%s';
-                                    break;
-                                }
-                            case 6 < (int)($column_data['length'] ?? 0): {
-                                    $column_data['type']    = ' mediumint(9)%s';
-                                    break;
-                                }
-                            case 4 < (int)($column_data['length'] ?? 0): {
-                                    $column_data['type']    = ' smallint(6)%s';
-                                    break;
-                                }
-                            case 0 < (int)($column_data['length'] ?? 0): {
-                                    $column_data['type']    = ' tinyint(4)%s';
-                                    break;
-                                }
-                            default:
-                                $column_data['type']    = ' int(11)%s';
-                        }
-                        $column_data['type'] = sprintf($column_data['type'], ($column_data['signed'] ?? true) ? '' : ' unsigned');
+                        $column_data['type'] = $this->create_integer_definition($column_data);
                         break;
                     }
                 case 'string': {
-                        switch (true) {
-                            case 10000   <   (int)($column_data['length'] ?? 0): {
-                                    $column_data['type']    = ' text';
-                                    break;
-                                }
-                            case 0   <   (int)($column_data['length'] ?? 0): {
-                                    $column_data['type']    = sprintf(' varchar(%d)', $column_data['length']);
-                                    break;
-                                }
-                            default:
-                                $column_data['type']    = ' varchar(100)';
-                        }
+                        $column_data['type'] = $this->create_string_definition($column_data);
                         break;
                     }
                 default:
                     $column_data['type'] = ' ' . $column_data['type'];
             }
-            switch ($column_data['default']) {
-            }
-            $autoincrement  = '';
+
+            $autoincrement      = '';
+
             if (!$has_auto_increment) {
                 $has_auto_increment =
                     $autoincrement  = ($column_data['autoincrement'] ?? false) ? ' AUTO_INCREMENT' : '';
@@ -114,14 +157,149 @@ class WPDB_DAO implements DAOConnector
                 ($column_data['null'] ?? true) ? ' NULL' : ' NOT NULL', // null
                 $autoincrement
             );
-            array_push($columns, $column);
+            array_push($column_list, $column);
+        }
+        return $column_list;
+    }
+
+
+    /**
+     * get model
+     * 
+     * create the model classname with prefixed namespace
+     * 
+     * @param   string
+     * @return  string|null
+     * @since   1.0.0
+     */
+    public function get_model(?string $tablename = null):?string
+    {
+        $model  = __NAMESPACE__.'\\'.ucfirst(strtolower($tablename ?? $this->table)).'_Model';
+        return $model;
+    }
+
+
+    /**
+     * get tablename
+     * 
+     * create the tablename with the required prefixes
+     * 
+     * @param   string
+     * @return  string|null
+     * @since   1.0.0
+     */
+    public function get_tablename(?string $tablename = null):?string
+    {
+        global $wpdb;
+
+        $tablename  = $wpdb->prefix . $this->prefix .( $tablename ?? $this->table);
+        return $tablename;
+    }
+
+
+    /**
+     * PUBLIC METHODS
+     */
+
+    /**
+     * constructor
+     * 
+     * @param   string
+     * @since   1.0.0
+     */
+    public function __construct(string $table)
+    {
+        $this->table = $table;
+    }
+
+    /**
+     * create row
+     * 
+     * @param   array $row
+     * @return  int|null
+     * @since   1.0.0
+     */
+    public function create(array $row):int {
+        global $wpdb;
+
+        $table_name = $this->get_tablename();
+        $res        = $wpdb->insert($table_name, $row);
+        if($res){
+            return $wpdb->insert_id;
+        }
+        return 0;
+    }
+
+
+    /**
+     * delete row
+     * 
+     * @param   array
+     * @return  bool
+     * @since   1.0.0
+     */
+    public function delete(array $row):bool {
+        return false;
+    }
+
+
+    /**
+     * read row 
+     * 
+     * @param   int|null
+     * @param   int|null
+     * @return  array|null
+     * @since   1.0.0
+     */
+    public function read($id = null, $page = null):?array
+    {
+        return array();
+    }
+
+
+    /**
+     * read row by query
+     * 
+     * @param   array
+     * @param   integer
+     * @return  array|null
+     * @since   1.0.0
+     */
+    public function read_by(array $query, $page = null):?array {
+        return null;
+    }
+
+
+    /**
+     * create table
+     * 
+     * create the table with the requirements from the dao
+     * 
+     * @param   array
+     * @return  int|null
+     * @since   1.0.0
+     */
+    public function create_table(array $data):?int
+    {
+        global $wpdb;
+
+        $model  = $this->get_model($data['tablename']);
+        $method = array($model,'create_table');
+        error_log(__CLASS__.'->'.__LINE__.'->'.is_callable($method).'_CLASS:'.$model);
+        if(is_callable($method)){
+            call_user_func($method, $data);
         }
 
-        /**
-         * database keys
-         */
-        $keys = array_merge($keys, $this->create_keys($data['keys'], 'KEY'));
-        $keys = array_merge($keys, $this->create_keys($data['unique_keys'], 'UNIQUE KEY'));
+        $table_name         = $this->get_tablename($data['tablename']);
+        $wpdb_collate       = $wpdb->collate;
+        $columns            =
+            $keys           = array();
+
+
+        $columns    = $this->create_columns($data['column_types']);
+
+        $keys       = array_merge($keys, $this->create_keys($data['keys'], 'KEY'));
+        $keys       = array_merge($keys, $this->create_keys($data['unique_keys'], 'UNIQUE KEY'));
 
         $sql = sprintf(
             'CREATE TABLE %s (
@@ -136,17 +314,39 @@ class WPDB_DAO implements DAOConnector
             $wpdb_collate
         );
 
+        // dbDelta is located in upgrade.php
         require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+
         $res = dbDelta($sql);
         return count($res);
     }
 
-    public function update(array $row) {}
 
-    public function delete(array $row) {}
+    /**
+     * updaterow
+     * 
+     * @param   array
+     * @return  int|null
+     * @since   1.0.0
+     */
+    public function update(array $row):?int {
+        return null;
+    }
 
-    public function table(string $table): DAOConnector
+
+
+    /**
+     * table
+     * 
+     * declare target table of the query
+     * 
+     * @param   array
+     * @return  DAOConnector
+     * @since   1.0.0
+     */
+    public function table(string $table): DaoConnectorInterface
     {
+        error_log(__CLASS__.'->'.__LINE__.'->Table:'.$table);
         $this->table = $table;
         return $this;
     }
