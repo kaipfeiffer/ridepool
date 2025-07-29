59b0b5f68e87341c8338d81ef8abf03cf716f083
Kai Pfeiffer <kp@loworx.com>
2025-07-29 20:32:55 +0200

    tamplate for main spage in backend tabs

---
+++ admin/templates/admin-start-template.php
@@ -0,0 +1,8 @@
+<div>
+    <h2>
+        <?php echo __('Manage data', 'ridepool'); ?>
+    </h2>
+    <p>
+        <?php echo __('Choose the tabs to manage the related data.', 'ridepool'); ?>
+    </p>
+</div>
